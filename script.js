343442d17d59b25c4d26884c2ddc3ba0d29d138f
elajman <elajman2000@gmail.com>
2024-12-08 18:23:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+// Configure Recurly
+recurly.configure('ewr1-MetLt2zdPoqf0mwqiB4taf');
+
+// Attach card element
+const elements = recurly.Elements();
+const cardElement = elements.CardElement();
+cardElement.attach('#recurly-elements');
+
+// Handle form submission
+document.getElementById('payment-form').addEventListener('submit', async function (e) {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    // Generate Recurly token
+    recurly.token(this, async function (err, token) {
+        if (err) {
+            alert('Payment failed: ' + err.message);
+            return;
+        }
+
+        // Set Recurly token
+        document.getElementById('recurly-token').value = token.id;
+
+        try {
+            // Generate reCAPTCHA v3 token
+            const recaptchaToken = await grecaptcha.execute('6LfwcrwmAAAAABmMABASDoYJNXJK2DjgPbSVV3xn', { action: 'payment' });
+
+            // Append reCAPTCHA token to the form data
+            const formData = new FormData(document.getElementById('payment-form'));
+            formData.append('RecaptchaToken', recaptchaToken);
+
+            // Send the form data to the server
+            const response = await fetch('/api/IzzyCheckoutApi/process-payment', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (!response.ok) {
+                const errorDetails = await response.text();
+                throw new Error(`Error: ${response.status}, Message: ${errorDetails}`);
+            }
+
+            const result = await response.json();
+            if (result.success) {
+                alert('Payment successful!');
+            } else {
+                alert(`Payment failed: ${result.message}`);
+            }
+        } catch (error) {
+            console.error('Error during fetch:', error);
+            alert(`An error occurred: ${error.message}`);
+        }
+    });
+});
